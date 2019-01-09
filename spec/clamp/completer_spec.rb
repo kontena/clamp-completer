32c11a6212cae01b18462894b2bff3ce2dfc77a6
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-01-09 10:59:57 +0200

    "Fix" the default spec

--- spec/clamp/completer_spec.rb
+++ spec/clamp/completer_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Clamp::Completer do
   it "has a version number" do
     expect(Clamp::Completer::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
